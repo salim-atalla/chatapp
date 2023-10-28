1c644ac243ae166bc1ca56430d8e0c8d70dc794d
salim-atalla <salimatalla.7@gmail.com>
2023-10-28 12:08:49 +0200

    Upload project files

---
+++ src/main/java/com/salim/chatapp/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.salim.chatapp.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ src/main/java/com/salim/chatapp/config/WebSocketEventListener.java
@@ -0,0 +1,36 @@
+package com.salim.chatapp.config;
+
+import com.salim.chatapp.chat.ChatMessage;
+import com.salim.chatapp.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private SimpMessageSendingOperations messageTemplate;
+
+    @EventListener
+    public void handleWebSocketDisconnectListener (SessionDisconnectEvent event) {
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
